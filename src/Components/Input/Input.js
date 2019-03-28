7f23390eef73b27afc553487863116923c6dbff8
katerina <katuha_maksimova@mail.ru>
2019-03-28 11:48:47 +0300

    add flag to change function

--- src/Components/Input/Input.js
+++ src/Components/Input/Input.js
@@ -19,6 +19,7 @@ class Input extends Component {
 
   handleChange = (event) => {
     this.setState({
+      dirty: true,
       value: event.target.value
     });
     this.props.handleChanges(event.target.value);
@@ -34,7 +35,7 @@ class Input extends Component {
               name={this.props.label}
               className="input__input"
               onChange={(event) => this.handleChange(event)}
-              value={!this.state.dirty ? this.state.value || this.props.initialValue : this.state.value}
+              value={this.state.dirty ? this.state.value : this.state.value || this.props.initialValue}
             />
             <button
               className={`input__clear`}
